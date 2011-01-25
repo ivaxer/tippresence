1755428ff29a3756310fa3f600ac092af9ed15e6
John Khvatov <ivaxer@imarto.net>
2011-01-25 13:40:58 +0300

    cleanup utils. Removed custom logging tools.

--- tippresence/utils.py
+++ tippresence/utils.py
@@ -4,14 +4,7 @@ from random import choice
 from string import ascii_letters
 
 from twisted.internet import reactor
-from twisted.python import log
-
-def seconds():
-    return reactor.seconds()
 
 def random_str(len):
     return "".join(choice(ascii_letters) for x in xrange(len))
 
-msg = log.msg
-warn = msg
-dbg = msg
