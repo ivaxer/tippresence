f059e2e98c406a99420d915edb59972e1606e250
John Khvatov <ivaxer@imarto.net>
2011-01-26 18:45:22 +0300

    completed setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,22 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 if __name__ == '__main__':
     setup(
             name='tippresence',
-            test_suite='tippresence.tests',
+            description='Presence server with SIP/HTTP interfaces written in Twisted Python',
+            license='MIT',
+            url = 'http://github.com/ivaxer/tippresence',
+            keywords = "SIP VoIP",
+
             author='John Khvatov',
             author_email='ivaxer@imarto.net',
+
+            install_requires = ['tipsip'],
+
+            packages = find_packages(),
+
+            test_suite='tippresence.tests',
+            package_data = {
+                'tippresence.amqp': ['amqp0-8.xml'],
+            }
     )
