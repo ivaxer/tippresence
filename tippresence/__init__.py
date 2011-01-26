bb325a5de1e7935ed69ee012d116ef61259743d5
John Khvatov <ivaxer@imarto.net>
2011-01-26 15:09:18 +0000

    export PresenceServiceError to tippresence

--- tippresence/__init__.py
+++ tippresence/__init__.py
@@ -1,6 +1,6 @@
 from statistics import Statistics
 stats = Statistics()
 
-from presence import PresenceService, Status
+from presence import PresenceService, PresenceServiceError, Status
 from presence import aggregate_status
 
